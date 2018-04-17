414215d4d6acc5841e4504326e5dd8155fa0f378
Uthaeus <romanlavery@gmail.com>
2018-04-17 09:56:32 -0600

    Exercise 03

---
+++ 03.js
@@ -0,0 +1,19 @@
+// 3. Write a JavaScript program to get the current date.  Go to the editor
+// Expected Output : 
+// mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+
+const today = new Date();
+var dd = today.getDays();
+var mm = today.getMonth();
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+console.log(`${dd}-${mm}-${yyyy}`);
