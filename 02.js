853d09d7526df806aa4659b6d6187afb2395dbd1
Uthaeus <romanlavery@gmail.com>
2018-04-17 09:51:58 -0600

    Exercise 02

---
+++ 02.js
@@ -0,0 +1,8 @@
+//2. Write a JavaScript program to print the contents of the current window.
+
+// For button in html code -> 
+// <button onclick="print_current_page()">Print this page</button>
+
+function print_current_page() {
+  window.print();
+}
