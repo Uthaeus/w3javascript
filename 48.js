ccc3ddfb813e18a4fbe7bca7b62c534a9c7633fe
Uthaeus <romanlavery@gmail.com>
2018-05-14 19:36:47 -0600

    Write a JavaScript program to reverse a given string.

---
+++ 48.js
@@ -0,0 +1,14 @@
+//  Write a JavaScript program to reverse a given string.
+
+
+
+function reverser(str) {
+  var test = str.split('').reverse().join('');
+
+  console.log(test);
+}
+
+
+// console.log(reverser('try this'));
+reverser('something');
+reverser('try this');
