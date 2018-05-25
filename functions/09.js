6a32f7e8d494d6d4bb9b4ce277888a55fdd46b35
Uthaeus <romanlavery@gmail.com>
2018-05-25 10:37:32 -0600

    Write a JavaScript function which accepts an argument and returns the type.

---
+++ functions/09.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function which accepts an argument and returns the type.
+
+
+const typer = arg => {
+  return typeof arg;
+}
+
+
+
+console.log(typer(true));
+console.log(typer('this'));
+console.log(typer(55));
