5c12a48bf7f5be80d38f57e3f49349bcebde7057
Uthaeus <romanlavery@gmail.com>
2019-11-12 10:23:33 -0700

    Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number

---
+++ 06_2.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar
+
+
+
+const leapYear = () => {
+    console.log("Enter Year (yyyy)");
+
+    let year = prompt("Enter Year", 'default');
+
+    return year;
+}
+
+
+console.log(leapYear());
