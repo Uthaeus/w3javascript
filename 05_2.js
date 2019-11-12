cefc01b754fff4c3db7c1f07491250185d66e2cc
Uthaeus <romanlavery@gmail.com>
2019-11-12 09:05:24 -0700

    periodically removing one letter from the end of the string and attaching it to the front

--- 05_2.js
+++ 05_2.js
@@ -1,3 +1,13 @@
 //  Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
 
+const go = () => {
+    let str = "w3resource";
 
+    setInterval(() => {
+        console.log(str);
+        str = str[str.length - 1] + str.substr(0, str.length - 1);
+
+    }, 500);
+}
+
+go();
